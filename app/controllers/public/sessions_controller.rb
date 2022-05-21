ae27dcf293c2cb6d4d316e16f218dc44e9eb1207
yuna-haru <skywar.love22@gmail.com>
2022-05-21 07:27:11 +0000

    [Finish]userトップページ、アバウトページ、新規登録、ログイン画面

---
+++ app/controllers/public/sessions_controller.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+
+class Public::SessionsController < Devise::SessionsController
+  # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
+
+  # GET /resource/sign_in
+  # def new
+  #   super
+  # end
+
+  # POST /resource/sign_in
+  # def create
+  #   super
+  # end
+
+  # DELETE /resource/sign_out
+  # def destroy
+  #   super
+  # end
+
+   protected
+
+   def guest_sign_in
+    user = User.guest
+    sign_in user
+    redirect_to public_path, notice: 'ゲストユーザーとしてログインしました。'
+   end
+
+  # If you have extra params to permit, append them to the sanitizer.
+  # def configure_sign_in_params
+  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+
+  namespace :public do
+    get '/' => 'homes#top'
+    get 'about' => 'homes#about', as:'about'
+  end
+
+  namespace :admin do
+  end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
