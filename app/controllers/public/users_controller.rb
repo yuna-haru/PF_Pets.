d65974e947cfe0770a4a878f3199038e3c812839
yuna-haru <skywar.love22@gmail.com>
2022-05-22 05:45:33 +0000

    [Start]user作成

--- app/controllers/public/users_controller.rb
+++ app/controllers/public/users_controller.rb
@@ -1,16 +1,34 @@
 class Public::UsersController < ApplicationController
 
+
+
+
+
   def show
     @user = User.find(params[:id])
     @post_images = @user.post_images
   end
 
   def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to public_user_path(@user.id)
   end
 
   def confirm
   end
 
   def withdraw
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
 end
