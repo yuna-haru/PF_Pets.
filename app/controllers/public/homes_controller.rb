e14d6256fdab5a32eb701cfc84bc648f769475e5
yuna-haru <skywar.love22@gmail.com>
2022-05-28 09:08:36 +0000

    [Add]topページの新着記事

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,12 @@
 class Public::HomesController < ApplicationController
   def top
+    @post_images = PostImage.last(4)
   end
 
-  
+
+  def post_image_params
+    params.require(:post_image).permit(:image)
+  end
+
+
 end
