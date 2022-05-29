f33c11a84cea1b413bb24857df03b9b346f04cd0
yuna-haru <skywar.love22@gmail.com>
2022-05-29 09:46:57 +0000

    [Finish]管理者投稿詳細画面

--- app/controllers/admin/post_images_controller.rb
+++ app/controllers/admin/post_images_controller.rb
@@ -5,8 +5,16 @@ class Admin::PostImagesController < ApplicationController
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
   end
 
   def edit
   end
+
+  private
+
+  def post_image_params
+    params.require(:post_image).permit(:title, :body, :image, :hashbody, hashtag_ids: [])
+  end
+
 end
