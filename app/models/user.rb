8efacced2f316d937055b48091e28c62c926820b
yuna-haru <skywar.love22@gmail.com>
2022-06-04 12:39:58 +0000

    [Finish]ユーザー側のレイアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,8 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :bookmarks, dependent: :destroy
   has_one_attached :profile_image
+  validates :name, presence: true
+
 
   def get_profile_image(width, height)
     unless profile_image.attached?
